39d15c050d1b80b9506c253d1e22efa295a0ba0c
Veronika Paulovicova <536510@mail.muni.cz>
2024-05-09 19:04:34 +0200

    Add basic data preparation.

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-# HERE will all the magic happen
+import requests
+from typing import Dict, List
+
+# Gets users based on jobs they are assigned to.
+def get_user_jobs(response: requests.Response) -> Dict[int, List[int]]:
+
+    count = 0
+    users: Dict[int, List[int]] = {}
+
+    for res in response.json()["results"]:
+        if res["assignee"]:
+            if res["assignee"]["id"] in users.keys():
+                users[res["assignee"]["id"]].append(res["task_id"])
+            else:
+                users[res["assignee"]["id"]] = res["task_id"]
+        count += 1
+    
+    # Prints count of all jobs (also those that are unassigned).
+    print(count)
+    return users
+
+
+def main() -> None:
+    # Getting the data from locally running server and printing the status code.
+    response = requests.get("http://localhost:8080/api/jobs")
+    print(response.status_code)
+
+    # Printing the aquired data in json format.
+    print(response.json())
+
+    # Some data preparation
+    users = get_user_jobs(response)
+    
+    for user in users.keys():
+        print(user, users[user])
+
+
+if __name__ == '__main__':
+    main()
