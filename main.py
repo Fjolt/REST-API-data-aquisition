cbbe17b3bef6b7573f8cace2a7a170f4f5f378c3
Veronika Paulovicova <536510@mail.muni.cz>
2024-05-09 21:12:29 +0200

    Adds logging utilisation.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 from typing import Dict, List
 import os
+import logging
 
 CSV_FOLDER_NAME = 'csv_data' # Name of the directory with csv files.
+SERVER_URL = 'http://localhost:8080/api/jobs' # Where our local server is running.
 
 
 # Gets users based on jobs they are assigned to.
@@ -23,10 +25,7 @@ def get_users(response: requests.Response) -> Dict[int, List[int]]:
 
         # If the job is unassigned, append him to the unassigned key
         users["Unassigned"].append(res["id"])
-
     
-    # Prints count of all jobs (also those that are unassigned).
-    print(len(response.json()["results"]))
     return users
 
 
@@ -63,25 +62,40 @@ def create_csv(user_id: int, user_jobs: List[int], jobs: Dict[int, tuple[str, st
 
 
 def main() -> None:
-    # Getting the data from locally running server and printing the status code.
-    response: requests.Response = requests.get("http://localhost:8080/api/jobs")
-    print(response.status_code)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filename="logs.log"
+    )
 
-    # Printing the aquired data in json format.
-    print(response.json())
+    # Getting the data from locally running server and printing the status code.
+    response: requests.Response = requests.get(SERVER_URL)
+    logging.info(f'Response Status Code: {response.status_code}')
+    
+    if(response.status_code != 200):
+        logging.error(f'Error while connecting to {SERVER_URL}. Finishing application.')
+        return
+    
+    # Prints response in json format in logs if DEBUG mode enbled.
+    logging.debug(f"Response JSON Data:\n {response.json()}")
 
     # Some data preparation
     users: Dict[int, List[int]] = get_users(response)
     jobs: Dict[int, tuple[str, str, str]] = get_jobs(response)
 
-    # Create the folder if it doesn't exist
+    # Creates the folder if it doesn't exist
     if not os.path.exists(CSV_FOLDER_NAME):
         os.makedirs(CSV_FOLDER_NAME)
-        print(f"Folder '{CSV_FOLDER_NAME}' created successfully.")
+        logging.info(f"Folder '{CSV_FOLDER_NAME}' created successfully.")
     
-    # Iterates through users and creates a file for each of them.
+    # Iterates through users and tries to create a file for each of them.
     for user in users.keys():
-        create_csv(user, users[user], jobs)
+        try:
+            create_csv(user, users[user], jobs)
+            logging.info(f"CSV file created for user: {user}.")
+        except Exception as e:
+            logging.error(f"Failed to create CSV file for user {user}: {str(e)}")
 
 
 if __name__ == '__main__':
